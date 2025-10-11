0cf75e7d212de39c051b9ac35da0fbe7f4c69dc3
shehan19-gif <shehanmaduwantha84@gmail.com>
2025-10-11 18:17:13 +0530

    CV changed

--- Components/downloadCV.js
+++ Components/downloadCV.js
@@ -1,6 +1,6 @@
 function downloadCV() {
-    const pathName = "./Assets/shehan_maduwantha_cv.pdf";
-    const fileName = "Shehan-CV.pdf";
+    const pathName = "./Assets/Shehan_Maduwantha_CV.pdf";
+    const fileName = "Shehan_Maduwantha_CV.pdf";
 
     const link = document.createElement("a");
     link.href = pathName;
