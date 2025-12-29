6ff58bdd3ca8f3c1aade51c7249533f23c41e4d2
shehan19-gif <shehanmaduwantha84@gmail.com>
2025-12-29 14:24:30 +0530

    Section Headings Changed

--- Components/projectList.js
+++ Components/projectList.js
@@ -4,10 +4,26 @@ let idNo = 1;
 export default [
     {
         id: idNo++,
+        title: "Expense & Todo Manager",
+        images: ["./tempAssets/Expense-Todo-App/1.JPG", "./tempAssets/Expense-Todo-App/2.JPG"],
+        languages: ["REACT", "CSS3", "NODEJS", "MONGODB", "GIT", "GITHUB"],
+        description: "Expense & Todo Manager <br /> CRUD Operations <br /> Cookies <br /> Middleware <br /> Forms Validation",
+        link: "https://shehan19-gif.github.io/expense-todo-app/"
+    },
+    {
+        id: idNo++,
+        title: "Url Shortener",
+        images: ["./tempAssets/Url-Shortener/url-1.JPG", "./tempAssets/Url-Shortener/url-2.JPG", "./tempAssets/Url-Shortener/url-3.JPG", "./tempAssets/Url-Shortener/url-4.JPG"],
+        languages: ["REACT", "CSS3", "NODEJS", "MONGODB", "GIT", "GITHUB"],
+        description: "Url Shortener <br /> Strict URL check <br /> URL redirection for original site",
+        link: "https://hop-link.vercel.app/"
+    },
+    {
+        id: idNo++,
         title: "AI Chat Application",
         images: ["./tempAssets/AI-Chat-App/ai-chat-app-01.JPG"],
         languages: ["REACT", "CSS3", "NODEJS"],
-        description: "=> AI chat bot using Gemini AI",
+        description: "AI chat bot using Gemini AI",
         link: "https://chat-free-with-gemini.netlify.app/"
     },
     {
@@ -15,7 +31,7 @@ export default [
         title: "Weather Application",
         images: ["./tempAssets/Weather-App/weather-01.JPG", "./tempAssets/Weather-App/weather-02.JPG", "./tempAssets/Weather-App/weather-03.JPG", "./tempAssets/Weather-App/weather-04.JPG"],
         languages: ["HTML5", "CSS3", "JAVASCRIPT"],
-        description: "-> Electron JS based application <br /> -> Based on a Weather API (Open Weather API)",
+        description: "Electron JS based application <br /> Based on a Weather API (Open Weather API)",
         link: "https://shehan19-gif.github.io/weather/"
     },
     {
@@ -23,7 +39,7 @@ export default [
         title: "Currency Converter Application",
         images: ["./tempAssets/Currency-App/currency1.JPG", "./tempAssets/Currency-App/currency2.JPG", "./tempAssets/Currency-App/currency3.JPG", "./tempAssets/Currency-App/currency4.JPG", "./tempAssets/Currency-App/currency5.JPG", "./tempAssets/Currency-App/currency6.JPG"],
         languages: ["HTML5", "CSS3", "JAVASCRIPT"],
-        description: "-> Real time data <br /> -> Based on a Currency API",
+        description: "Real time data <br /> Based on a Currency API",
         link: "https://currency-converter-of4o.onrender.com/"
     },
     {
@@ -31,7 +47,7 @@ export default [
         title: "Travel World",
         images: ["./tempAssets/Travel-World/travel-1.JPG", "./tempAssets/Travel-World/travel-2.JPG", "./tempAssets/Travel-World/travel-3.JPG", "./tempAssets/Travel-World/travel-4.JPG", "./tempAssets/Travel-World/travel-5.JPG", "./tempAssets/Travel-World/travel-6.JPG"],
         languages: ["REACT", "CSS3"],
-        description: "-> Countries with their main cities <br /> -> Main cities travel guide videos integrating with YouTube",
+        description: "Countries with their main cities <br /> Main cities travel guide videos integrating with YouTube",
         link: "https://68675471d8f47b1f3c773e2c--travel-world-new.netlify.app/"
     },
     {
@@ -39,15 +55,7 @@ export default [
         title: "Portfolio Web",
         images: ["./tempAssets/Portfolio-Web/portfolio-1.JPG", "./tempAssets/Portfolio-Web/portfolio-2.JPG"],
         languages: ["REACT", "CSS3"],
-        description: "-> Personal portfolio <br /> -> Browser routing",
+        description: "Personal portfolio <br /> Browser routing",
         link: "https://shehan19-gif.github.io/react-portfolio/"
-    },
-    {
-        id: idNo++,
-        title: "Url Shortener",
-        images: ["./tempAssets/Url-Shortener/url-1.JPG", "./tempAssets/Url-Shortener/url-2.JPG", "./tempAssets/Url-Shortener/url-3.JPG", "./tempAssets/Url-Shortener/url-4.JPG"],
-        languages: ["REACT", "CSS3", "NODEJS", "MONGODB", "GIT", "GITHUB"],
-        description: "-> Url Shortener <br /> -> Strict URL check <br /> -> URL redirection for original site",
-        link: "https://hop-link.vercel.app/"
     }
 ]
